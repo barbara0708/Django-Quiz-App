635677b7504c7062aab52324f32484f8e74bfd97
barbara <marcelineabadir527@gmail.com>
2023-06-06 20:18:19 +0200

    new commit

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path("",views.index,name='index'),
     path('categories/',views.categories,name="categories"),
     path('categories/category/',include('category.urls')),
     path('sign-up/',views.signup_view,name="sign-up"),
-    path('login/',views.login_view,name="login")
+    path('login/',auth_views.LoginView.as_view(template_name='core/login.html'),name="login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name='core/logout.html'),name='logout')
 ]
