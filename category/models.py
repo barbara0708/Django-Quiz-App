bee1d0f76ff79f7c1cbb125da1c388cf93c20bee
barbara <marcelineabadir527@gmail.com>
2023-06-07 20:23:02 +0200

    models

--- category/models.py
+++ category/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from core.models import Categories
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quiz(models.Model):
+    quiz_name=models.CharField(max_length=150)
+    desc=models.CharField(max_length=200)
+    category=models.ForeignKey(Categories,on_delete=models.CASCADE)
+    url=models.SlugField(unique=True)
+    def __str__(self) :
+        return str(self.quiz_name)
+
+class Question(models.Model):
+    quiz=models.OneToOneField(Quiz,related_name='question',on_delete=models.CASCADE)
+    content=models.CharField(max_length=200)
+    def __str__(self):
+        return str(self.content) 
+
+class Scores(models.Model):
+    user_id=models.OneToOneField(User,related_name='score',on_delete=models.CASCADE)
+    quiz_id=models.OneToOneField(Quiz,related_name='score',on_delete=models.CASCADE)
+    points=models.IntegerField()
+
+class Answer(models.Model):
+    question=models.OneToOneField(Question,on_delete=models.CASCADE)
+    content=models.CharField(max_length=200)
+    correct=models.BooleanField(default=False)
+    def __str__(self):
+        return str(self.content)
+
--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-
-#class User(AbstractUser):
-#    birthday=models.DateField(null=False, blank=True)
-    
+class Categories(models.Model):
+    name=models.CharField(max_length=100)
+    slug=models.SlugField(unique=True)
+    def __str__(self) :
+        return str(self.name)
