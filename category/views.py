a9f8b738b836ab835ee0740c95ab241f87525974
barbara <marcelineabadir527@gmail.com>
2023-06-08 17:47:47 +0200

    models

--- category/views.py
+++ category/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Quiz
+from core.models import Categories
 
 # Create your views here.
 def quizes(request,slug):
-    return render(request,'category/quizes.html')
+    cID=Categories.objects.get(slug=slug)
+    all_quizes=Quiz.objects.get(category_id=cID.id)
+    return render(request,'category/quizes.html',context={'quizes':all_quizes})
 
 def quiz(request,slug,id):
     return render(request,'category/quiz.html')
