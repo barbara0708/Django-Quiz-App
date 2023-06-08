d12f27e5bc79f827c92009f63befc76ee4148715
barbara <marcelineabadir527@gmail.com>
2023-06-08 08:48:19 +0200

    models

--- category/models.py
+++ category/models.py
@@ -3,28 +3,33 @@ from core.models import Categories
 from django.contrib.auth.models import User
 
 class Quiz(models.Model):
-    quiz_name=models.CharField(max_length=150)
-    desc=models.CharField(max_length=200)
-    category=models.ForeignKey(Categories,on_delete=models.CASCADE)
-    url=models.SlugField(unique=True)
+    quiz_name=models.CharField(max_length=150,blank=True,null=True)
+    desc=models.CharField(max_length=200,blank=True,null=True)
+    category=models.ForeignKey(Categories,on_delete=models.CASCADE,blank=True,null=True)
+    url=models.SlugField(unique=True,blank=True,null=True)
     def __str__(self) :
         return str(self.quiz_name)
 
 class Question(models.Model):
-    quiz=models.OneToOneField(Quiz,related_name='question',on_delete=models.CASCADE)
-    content=models.CharField(max_length=200)
+    quiz=models.OneToOneField(Quiz,related_name='question',on_delete=models.CASCADE,blank=True,null=True)
+    content=models.CharField(max_length=200,blank=True,null=True)
     def __str__(self):
         return str(self.content) 
 
 class Scores(models.Model):
-    user_id=models.OneToOneField(User,related_name='score',on_delete=models.CASCADE)
-    quiz_id=models.OneToOneField(Quiz,related_name='score',on_delete=models.CASCADE)
-    points=models.IntegerField()
+    user_id=models.OneToOneField(User,related_name='score',on_delete=models.CASCADE,blank=True,null=True)
+    quiz_id=models.OneToOneField(Quiz,related_name='score',on_delete=models.CASCADE,blank=True,null=True)
+    points=models.IntegerField(blank=True,null=True)
+    passed=models.BooleanField(default=False,blank=True,null=True)
+    def __str__(self):
+        return str(self.points) 
+
+
 
 class Answer(models.Model):
-    question=models.OneToOneField(Question,on_delete=models.CASCADE)
-    content=models.CharField(max_length=200)
-    correct=models.BooleanField(default=False)
+    question=models.OneToOneField(Question,on_delete=models.CASCADE,blank=True,null=True)
+    content=models.CharField(max_length=200,blank=True,null=True)
+    correct=models.BooleanField(default=False,blank=True,null=True)
     def __str__(self):
         return str(self.content)
 
