db00f3f310c707b9432900d0f685e5cfcf5edae5
barbara <marcelineabadir527@gmail.com>
2023-06-12 12:16:16 +0200

    quiz

--- quizes/settings.py
+++ quizes/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'category',
     'bootstrap5',
     'crispy_forms',
-    'crispy_bootstrap5',
-    'pillow'
+    'crispy_bootstrap5'
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
