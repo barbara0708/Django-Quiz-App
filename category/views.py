47938c2aa4e8cbf09bfc15f1cb208d00a3acdaca
barbara <marcelineabadir527@gmail.com>
2023-06-12 13:52:15 +0200

    quizes

--- category/views.py
+++ category/views.py
@@ -7,8 +7,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def quizes(request,slug):
     cID=Categories.objects.get(slug=slug)
-    all_quizes=Quiz.objects.get(category_id=cID.id)
-    return render(request,'category/quizes.html',context={'quizes':all_quizes})
+    all_quizes=Quiz.objects.filter(category_id=cID.id)
+    return render(request,'category/quizes.html',context={'quizzes':all_quizes})
 
 @login_required
 def quiz(request,url,slug):
