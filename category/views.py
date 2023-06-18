e6b5df40cc823184525027f6959707bc4ee441e4
barbara <marcelineabadir527@gmail.com>
2023-06-18 11:00:18 +0200

    quiz

--- category/views.py
+++ category/views.py
@@ -34,11 +34,9 @@ def quiz(request,url,slug):
         return render(request,'category/quiz.html',context)
 
     if request.method=='POST':
-        body=request.session.get('3')
-        print(body)
-        # body_unicode=request.body.decode('utf-8')
-        # #body=json.loads(body_unicode)
-        # print("reques body: ",body_unicode)
+        body_unicode=request.body.decode('utf-8')
+        body=json.loads(body_unicode)
+        print("reques body: ",body)
         #parse_json = json.loads(request.body)
         #print(parse_json.items())
         # for k,v in parse_json.items():
