774ac26984b00a3f5d990805b2f8167eb79fe8e9
barbara <marcelineabadir527@gmail.com>
2023-07-08 12:55:13 +0200

    db & media

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Categories
+from .models import Categories, UserInfo
 
 admin.site.register(Categories)
+admin.site.register(UserInfo)
+
--- core/models.py
+++ core/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class Categories(models.Model):
     name=models.CharField(max_length=100)
     slug=models.SlugField(unique=True, default=uuid.uuid1)
     def __str__(self) :
         return str(self.name)
+
+class UserInfo(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,default=uuid.uuid1)
+    profile_pic=models.ImageField(upload_to='images/profile_pictures/',blank=True,null=True)
+    def __str__(self) :
+        return self.user.username
