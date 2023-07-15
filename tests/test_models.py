59fbda9f830c80ca74e6077a9217a32e6f6fff9f
barbara <marcelineabadir527@gmail.com>
2023-07-15 09:01:10 +0200

    tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,3 +11,12 @@ class QuizClassTest(TestCase):
         label=quiz._meta.get_field('quiz_name').verbose_name
         self.assertEqual(label,'quiz name')
     
+    def check_category_label(self):
+        quiz=Quiz.objects.get(id=1)
+        label=quiz._meta.get_field('category').verbose_name
+        self.assertEqual(label,'category')
+
+    def quiz_name_max_length(self):
+        quiz=Quiz.objects.get(id=1)
+        max_length=quiz._meta.get_field('quiz_name').max_length
+        self.assertEqual(max_length,'100')
